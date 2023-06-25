f13993577dca25f077014d8b7301cc4f1a13f81c
Syahfitria Syarifuddin <syh.syarifuddin@gmail.com>
2023-06-25 11:08:29 +0700

    Fix render json with add key for data sleep record

--- app/controllers/sleep_records_controller.rb
+++ app/controllers/sleep_records_controller.rb
@@ -3,7 +3,7 @@ class SleepRecordsController < ApplicationController
   def index
     get_sleep_records = SleepRecord.all_clock
     data = SleepRecordSerializer.instance.serialize(get_sleep_records)
-    render_json(data)
+    render_json(data: data)
   end
 
 end
