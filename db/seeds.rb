3ce5e8922a2d8781194dd380950d403fcfa3dace
Syahfitria Syarifuddin <syh.syarifuddin@gmail.com>
2023-06-25 15:21:01 +0700

    Fix duration sum

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ user2 = User.create(name: "Rick")
 user3 = User.create(name: "Maria")
 
 # Create some sleep records for the users
-user1.sleep_records.create(start_time: Date.today.prev_week.at_beginning_of_week, end_time: 4.hours.ago, duration: start_of_previous_week - 4.hours.ago)
-user1.sleep_records.create(start_time: Date.today.prev_week.at_beginning_of_week, end_time: 6.hours.ago, duration: start_of_week - 6.hours.ago)
-user2.sleep_records.create(start_time: Date.today.prev_week.at_beginning_of_week, end_time: 3.hours.ago, duration: 7.hours.ago - 3.hours.ago)
-user3.sleep_records.create(start_time: Date.today.prev_week.at_beginning_of_week, end_time: 9.hours.ago, duration: 12.hours.ago - 9.hours.ago)
+user1.sleep_records.create(start_time: Date.today.prev_week.at_beginning_of_week, end_time: 4.hours.ago, duration: 2.hours.ago - 4.hours.ago)
+user1.sleep_records.create(start_time: Date.today.prev_week.at_beginning_of_week, end_time: 6.hours.ago, duration: 1.hours.ago - 6.hours.ago)
+user2.sleep_records.create(start_time: Date.today.prev_week.at_beginning_of_week, end_time: 3.hours.ago, duration: 1.hours.ago - 3.hours.ago)
+user3.sleep_records.create(start_time: Date.today.prev_week.at_beginning_of_week, end_time: 9.hours.ago, duration: 6.hours.ago - 9.hours.ago)
